7a024dab8f5eb8e79f33f78ee4f7561c939ce1db
Anime4000 <ilham92_sakura@yahoo.com>
2015-07-26 04:30:09 +0800

    Make it runable

--- nemupad/Program.cs
+++ nemupad/Program.cs
@@ -13,9 +13,28 @@ namespace nemupad
 		[STAThread]
 		static void Main(string[] args)
 		{
+			string file;
+			string lang;
+
+			if (args.Length > 1)
+			{
+				file = args[1];
+				lang = args[0];
+			}
+			else if (args.Length > 0)
+			{
+				file = args[0];
+				lang = "eng"; // load default
+			}
+			else
+			{
+				file = null;
+				lang = "eng"; // load default
+			}
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new frmMain(args[1], args[0]));
+			Application.Run(new frmMain(file, lang));
 		}
 	}
 }
